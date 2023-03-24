35b45a4e3d5e0a7d00eac8acbfc0293d8538be46
luczwyssig <77100623+thefaceslayer12@users.noreply.github.com>
2023-03-24 17:04:04 +0100

    react-app mit router angefangen zu konfiguieren

---
+++ versicherungsrechner/src/Home.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+function Home () {
+    return(
+        <div>
+            test
+        </div>
+    );
+    
+
+    
+
+
+}
+
+export default Home;
