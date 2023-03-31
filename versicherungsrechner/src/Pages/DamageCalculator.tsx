ee5365fd1503d01bcd8d3243a5a160f5eaaeeac6
luczwyssig <77100623+thefaceslayer12@users.noreply.github.com>
2023-03-31 14:51:10 +0200

    rechner abgeschlossen, bootstrap installiert

--- versicherungsrechner/src/Pages/DamageCalculator.tsx
+++ versicherungsrechner/src/Pages/DamageCalculator.tsx
@@ -3,14 +3,19 @@ import ReactDOM from 'react-dom/client';
 import { Link } from 'react-router-dom';
 import Header from '../Header';
 import Instruction from '../Comps/Instructions';
+ // change variable names to english
 
 function Calculator () {
     const [Versumme, setVersumme] = useState(0);
-    const [Schaden, setSchaden] = useState(0)
+    const [Schaden, setSchaden] = useState(0);
+    const [Hauswert, setHausw] = useState(0);
+    const [damage, setdamage] = useState(0);
+
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        
+       setdamage(Versumme/Hauswert*Schaden);
       };
+      
       
     
     
@@ -20,10 +25,13 @@ function Calculator () {
           <Instruction shorttext='shorttest' longtext='longtest' />
           <form onSubmit={handleSubmit}>
             <input type="number" placeholder="Versicherungssumme eingeben" onChange={event => setVersumme(event.target.valueAsNumber)}/>
-            <input type="text" placeholder="Schaden eingeben" />
-            <input type="text" placeholder="Hauswert eingeben" />
+            <input type="number" placeholder="Schaden eingeben" onChange={event => setSchaden(event.target.valueAsNumber)}/>
+            <input type="number" placeholder="Hauswert eingeben" onChange={event => setHausw(event.target.valueAsNumber)}/>
             <input type="submit" value="submit" />
          </form>
+         <div>
+            {damage}
+         </div>
         
         </div>
       );
