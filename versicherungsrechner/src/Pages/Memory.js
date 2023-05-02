e014a4bb57f7f49c56b0d13ee1125dc9dd63354e
luczwyssig <77100623+thefaceslayer12@users.noreply.github.com>
2023-05-02 14:32:42 +0200

    mit memory abgebrochen, war zu schwierig. werde mit einem einfacheren spiel anfangen

--- versicherungsrechner/src/Pages/Memory.js
+++ versicherungsrechner/src/Pages/Memory.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import { Container } from 'react-bootstrap';
 import Header from '../Comps/Header.js';
@@ -8,42 +8,19 @@ import Row from 'react-bootstrap/esm/Row';
 import Col from 'react-bootstrap/esm/Col';
 import Popuper from '../Comps/Popuper.js';
 import Footer from '../Comps/Footer.js'
+import  Memitem from '../Comps/Memoryitem.js';
 
 
-function Memory(){
-    const terms =["Versicherungssumme", "Hauswert", "Hausrat", "Prämie", "Überversicherung", "Unterversicherung"];
-    const definitions = ["1", "2", "3", "4", "5", "6"];
-    const shuffledarray = terms.concat(definitions).sort(function () {
-        return Math.random() -0.3;
-    });
-    function check(){
-        const first = null;
+function Game(){
 
-    }
+    const terms = [{
+        quest: "a",
+        ans: "A"
+    },
     
-    
-    return(
-        <div>
-            <Row>
-                <Col>{shuffledarray[0]}</Col>
-                <Col>{shuffledarray[1]}</Col>
-                <Col>{shuffledarray[2]}</Col>
-                <Col>{shuffledarray[3]}</Col>
-            </Row>
-            <Row>
-                <Col>{shuffledarray[4]}</Col>
-                <Col>{shuffledarray[5]}</Col>
-                <Col>{shuffledarray[6]}</Col>
-                <Col>{shuffledarray[7]}</Col>
-            </Row>
-            <Row>
-                <Col>{shuffledarray[8]}</Col>
-                <Col>{shuffledarray[9]}</Col>
-                <Col>{shuffledarray[10]}</Col>
-                <Col>{shuffledarray[11]}</Col>
-            </Row>
 
-        </div>
-    )
+    ]
+
+
 }
-export default Memory;
+export default Game;
